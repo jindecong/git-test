94580dfeb8de8d790e02cd29edb248843c5f404d
jess <1396145334@qq.com>
2020-11-04 22:31:15 +0800

    把本地工程提添加到git

---
+++ git-test-project/src/com/jess/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.jess.git;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
